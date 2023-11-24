e7b94bed08f8f167fa71606abc57956efbff3b1e
vhafener <vhafener@Outlook.com>
2023-11-24 12:21:27 -0500

    Shortened and fixed collectSacctDB.sh

--- collectSacctDB.sh
+++ collectSacctDB.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 sinfo --version > slurm_version
-GENERATED=Generated:`date +%FT%T`
 
-printf "db initialized 2019-12-01\n"
 CLUSTERNAME=$(scontrol show config | awk '/ClusterName/ {print $NF}')
 OUT=sacct.out.${CLUSTERNAME}.start=2023-10-01T00:00.no-identifiers.txt
 FIELDS="jobidraw%9,priority,qos,partition,nnodes%6,ntasks,submit%24,eligible%24,start%24,end%24,timelimit%24,state,reservation,nodelist,flags%36"
@@ -11,8 +9,6 @@ ARGS="-a -X -p"
 START="--start=2023-10-01T00:00"
 
 time sacct --format=${FIELDS} ${ARGS} ${START} --delimiter=/ > ${OUT}
-
-#chmod 0444 slurm_version  ${OUT}
 
 sed 's/,/|/g' ${OUT} > out.txt
 sed 's|/|,|g' out.txt > ${OUT}
