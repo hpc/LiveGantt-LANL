8a00d60e5bae2aab41a2f729aa01602a1d629401
vhafener <vhafener@lanl.gov>
2024-02-13 12:39:02 -0700

    Building prereqs for scrontab

---
+++ SBATCH_LIVEGANTT.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Submit this script with: sbatch filename
+
+#SBATCH --time=:30:00   # walltime
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --job-name=LiveGantt   # job name
+#SBATCH --account=vhafener   # account name
+#SBATCH --qos=hpcdev   # qos name
+#SBATCH --no-requeue   # do not requeue when preempted and on node failure
+#SBATCH --signal=23@60  # send signal to job at [seconds] before end
+
+
+# LOAD MODULEFILES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+current_date=$(date +'--start=%Y-%m-%dT00:00')
+outfile="sacct.out.$(scontrol show config | awk '/ClusterName/ {print $NF}').start=$(date +'%Y-%m-%d')T00:00.no-identifiers.txt"
+bundle="/users/vhafener/LiveGantt/oci_images/livegantt_v0.6/rootfs"
+
+./collectSacctSB.sh  &
+
+# Capture the process ID of the last background command
+pid_collect=$!
+
+# Wait for a.sh to finish
+wait $pid_collect
+
+# Launch b.sh after a.sh has finished
+srun --container $bundle bash -c "cat /etc/*rel*"
--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-VERSION="0.4"
+VERSION="0.6"
+FOLDER_ROOT="/Users/vhafener/Repos/LiveGantt"
+#FULL_NAME=livegantt_v${VERSION}
 docker build -t livegantt_v${VERSION} .
-docker save livegantt_v${VERSION} > livegantt_v${VERSION}.tar
+mkdir -p ${FOLDER_ROOT}/oci_images/livegantt_v${VERSION}/rootfs
+docker create --name livegantt_v${VERSION} livegantt_v${VERSION}
+docker export livegantt_v${VERSION} | tar -C ${FOLDER_ROOT}/oci_images/livegantt_v${VERSION}/rootfs -xf -
+docker rm livegantt_v${VERSION}
