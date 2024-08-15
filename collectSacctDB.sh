676fe5a568d7c35df42beafd7781e0ea077b7d9e
vhafener <vhafener@lanl.gov>
2024-08-15 10:33:56 -0600

    Consolidated sed commands

--- collectSacctDB.sh
+++ collectSacctDB.sh
@@ -14,13 +14,8 @@ OUT="sacct.out.${CLUSTERNAME}.start=${1:-$(date +'%Y-%m-%d')}T00:00.no-identifie
 
 time sacct --format=${FIELDS} ${ARGS} ${START} --delimiter=∆ > ${OUT}
 
-#chmod 0444 slurm_version  ${OUT}
-
-# TODO These can be the same, right?
-sed 's/,/|/g' ${OUT} > out.txt
-sed 's|∆|,|g' out.txt > out2.txt
-iconv -c -f us-ascii -t UTF-8//TRANSLIT out2.txt > ${OUT}
+sed -e 's/,/|/g' -e 's|∆|,|g' ${OUT} > out.txt
+iconv -c -f us-ascii -t UTF-8//TRANSLIT out.txt > ${OUT}
 rm out.txt
-rm out2.txt
 
 #python3 src/__main__.py -i${OUT} -t36 -n${CLUSTERNAME} -c368
