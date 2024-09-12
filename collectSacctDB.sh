8d351f10521d3283184a26fda2aa2c7d17d49391
Steven Senator <sts@lanl.gov>
2024-09-12 12:47:32 -0600

    exit codes, json optional

--- collectSacctDB.sh
+++ collectSacctDB.sh
@@ -7,7 +7,7 @@
 #
 # trigger with, for example:
 # crontab entry:
-#    37 3,16 1,16 * * sh -c "[ -x ${SACCT_DIR}/bin/collectSacctDB.sh ] && \
+#    37 3 1,16 * * sh -c "[ -x ${SACCT_DIR}/bin/collectSacctDB.sh ] && \
 #             ( cd ${SACCT_DIR}/$(hostname -s); ${SACCT_DIR}/bin/collectSacctDB.sh )"
 #
 # To Do:
@@ -16,8 +16,15 @@
 #     - set SACCT_DIR
 #     - set DEFAULT_INITIALIZED date
 #     - set timeouts, esp. for json sacct query
+#     - set breadcrumb, previous timeout used, esp. if previous run had no json output
+#     - check for timeout_long breadcrumb, increase from TIMEOUT_LONG if found
+#     - SKIP_JSON=false
 #   - verify env
 #     - slurm env & commands appear reasonable
+#   - refine collect/timeout/wait
+#     - slice query into smaller time chunks if no json output in a TIMEOUT period
+#     - instead of sleeping, poll for output, possibly warn, if no progress made
+#       then if no json ever collected, leave timeout_long breadcrumb
 
 set -e
 set -u
@@ -48,14 +55,30 @@ findEarliestDBJobRecord() {
 SLURM_VERSION="SlurmVersion"
 TSTAMP_YMD=`date +%Y-%b-%d`
 TSTAMP=`date +%FT%T`
-TIMEOUT_SHORTLONG_MULTIPLIER=10
-TIMEOUT_SHORT=1
+
+SKIP_JSON="true"
+
+TIMEOUT_SAFE_MIN=5
+# tune for your db responsiveness and size (TIMEOUT_SHORTLONG_MULTIPLIER)
+TIMEOUT_SHORTLONG_MULTIPLIER=20
+TIMEOUT_SHORT=${TIMEOUT_SAFE_MIN}
 TIMEOUT_LONG=$((${TIMEOUT_SHORT} * ${TIMEOUT_SHORTLONG_MULTIPLIER}))
 TIMEOUT_UNITS="m"
 TIMEOUT=${TIMEOUT_SHORT}${TIMEOUT_UNITS}
 TIMEOUT_LONG=${TIMEOUT_LONG}${TIMEOUT_UNITS}
+
+#Exit codes
+EX_OK=0
+EX_TXTJSON_MISMATCH=1
+EX_TXTDELIM_MISMATCH=2
+EX_JSONDELIM_MISMATCH=3
+EX_OVERWRITE=10
+EX_INCOMPLETE=11
+EX_PREP_DATE=20
+
 TMPDIR=/tmp/$(basename $0 .sh).$(id -u -n).$$
 TMPFILE=${TMPDIR}/delim-conv.$$
+
 MSG_INCOMPLETE="output not complete"
 MSG_JOBID_MISMATCH="jobid mismatch in output formats"
 GENERATED="Generated"
@@ -70,6 +93,7 @@ DELTA="∆"
 COMMA=","
 SINGLE_QUOTE="'"
 READ_ONLY="0444"
+ISATTY=""
 
 FIELDS="jobidraw%9,jobid,priority,qos,partition,nnodes%6,ntasks,submit%24,eligible%24,start%24,end%24,timelimit%24,state,reservation,nodelist,flags%36,submitline%-7,account,user,consumedenergy,consumedenergyraw,failednode%-80"
 ARGS=" -a -X "
@@ -83,26 +107,35 @@ START="${START_PREFIX}${INITIALIZED}${START_SUFFIX}"
 
 
 Prep() {
+	if [[ -c /dev/tty ]] ; then
+		ISATTY=">/dev/tty"
+	fi
 	# sanity check
 	date -d "${INITIALIZED}" +%s >/dev/null 2>&1
-	if [ $? -ne 0 ] ; then
-		echo date failure determining when the db was initialized. >/dev/tty
-		exit 1
+	if [ $? -ne ${EX_OK} ] ; then
+		echo date failure determining when the db was initialized. ${ISATTY}
+		exit ${EX_PREP_DATE}
 	fi
 	if [ -z "${INITIALIZED}" ] ; then
 		INITIALIZED=${DEFAULT_INITIALIZED}
-		echo initialized: ${DEFAULT_INITIALIZED} >/dev/tty
+		echo initialized: ${DEFAULT_INITIALIZED} ${ISATTY}
 	fi
 	mkdir -p ${TMPDIR}
 }
 
 CollectSlurmData() {
-	# json takes much longer
-	DEBUG_TIME="/usr/bin/time -v"
-	timeout ${TIMEOUT_LONG} ${DEBUG_TIME}	sacct ${JSON_ARGS} ${START}			> ${OUT_JSON}	&
-	timeout ${TIMEOUT}			sacct --format=${FIELDS} ${ARGS} ${START}	> ${OUT}	&
-	timeout ${TIMEOUT}			sacct --format=${FIELDS} ${P_ARGS} ${START}	> ${OUT_DELIM}	&
+	# json takes much (much) longer
+	#DEBUG_TIME="/usr/bin/time -v"
+	# timeout ${TIMEOUT_LONG} ${DEBUG_TIME}	sacct ${JSON_ARGS} ${START} > ${OUT_JSON} &
+	if [[ -z "${SKIP_JSON}" ]] ; then
+                nohup timeout ${TIMEOUT_LONG} sacct ${JSON_ARGS} ${START} > ${OUT_JSON}	&
+	fi
+
+	# these don't take very long
+	timeout ${TIMEOUT} sacct --format=${FIELDS} ${ARGS} ${START}	> ${OUT} &
+	timeout ${TIMEOUT} sacct --format=${FIELDS} ${P_ARGS} ${START}	> ${OUT_DELIM} &
 	wait
+	return
 }
 
 ConvertDelimiters() {
@@ -110,72 +143,78 @@ ConvertDelimiters() {
 	sed "s|${DELTA}|${COMMA}|g" ${TMPFILE}				> ${OUT_DELIM}${DELTA}
 	iconv -c -f us-ascii -t UTF-8//TRANSLIT ${OUT_DELIM}${DELTA}	> ${OUT_DELIM}
 	rm -f ${TMPFILE} ${OUT_DELIM}${DELTA}
+	return
 }
 
 CheckValid() {
-	local rc=0
+	local rc=${EX_OK}
+	local last_jobid_json=""
 	last_jobid_txt=$(tail -1 ${OUT} | awk '{print $1;}')
 	last_jobid_delim=$(tail -1 ${OUT_DELIM} | awk -F "${COMMA}" '{print $1;}' | sed "s/${SINGLE_QUOTE}//g" )
 
 	# XXX JSON *not* fatal: See SchedMD ticket# 20797, https://support.schedmd.com/show_bug.cgi?id=20797
-	last_jobid_json=$(tail -${JSON_TAIL_LINES} ${OUT_JSON} | grep job_id | grep -v '"job_id": 0,' | sed 's/,$//' | sort | uniq | awk '{print $2;}' )
+	if [[ -z "${SKIP_JSON}" ]] ; then
+	        last_jobid_json=$(tail -${JSON_TAIL_LINES} ${OUT_JSON} | grep job_id | grep -v '"job_id": 0,' | sed 's/,$//' | sort | uniq | awk '{print $2;}' )
 
-
-	if [[ "${last_jobid_txt}" != "${last_jobid_json}" ]] ; then
-		(\
-                 echo ${MSG_JOBID_MISMATCH} 		;\
-                 echo "	txt:	${last_jobid_txt}"	;\
-                 echo "	json:	${last_jobid_json}" ) | tee ${WARNING}txt,json
-		 rc=1
-	 fi
+        	if [[ -n "${last_jobid_json}" ]] ; then
+        		if [[ "${last_jobid_txt}" != "${last_jobid_json}" ]] ; then
+        			(\
+                                 echo ${MSG_JOBID_MISMATCH} 		;\
+                                 echo "	txt:	${last_jobid_txt}"	;\
+                                 echo "	json:	${last_jobid_json}" ) | tee ${WARNING}txt,json
+                                rc=${EX_TXTJSON_MISMATCH}
+                        fi
+	                if [[ "${last_jobid_json}" != "${last_jobid_delim}" ]] ; then
+		                (\
+                                 echo ${MSG_JOBID_MISMATCH}		;\
+                                 echo "	json:	${last_jobid_json}"	;\
+                                 echo "	delim:	${last_jobid_delim}"	) | tee ${WARNING}json,delim
+		                rc=${EX_JSONDELIM_MISMATCH}
+	                fi
+	        else
+		        # ...for TIMEOUT_LONG
+		        wait
+	        fi
+	fi ## SKIP_JSON
 
 	if [[ "${last_jobid_txt}" != "${last_jobid_delim}" ]] ; then
 	       (\
                 echo ${MSG_JOBID_MISMATCH}		;\
                 echo "	txt:	${last_jobid_txt}"	;\
                 echo "	delim:	${last_jobid_delim}"	) | tee ${WARNING}txt,delim
-	       rc=2
-	fi
-	if [[ "${last_jobid_json}" != "${last_jobid_delim}" ]] ; then
-		(\
-                 echo ${MSG_JOBID_MISMATCH}		;\
-                 echo "	json:	${last_jobid_json}"	;\
-                 echo "	delim:	${last_jobid_delim}"	) | tee ${WARNING}json,delim
-		rc=3
+               rc=${EX_TXTDELIM_MISMATCH}
 	fi
 
-	if [[ "${rc}" -ne 0 ]] ; then
-		return ${rc}
-	fi
+	if [[ "${rc}" -eq ${EX_OK} ]] ; then
+                if [[ -e ${TSTAMP_YMD}/${GENERATED} ]] ; then
+                        rc=${EX_OVERWRITE}
+                else
+		        # if incomplete or in error, leave behind all of the breadcrumb temporary files
+		        # we record the slurm version as the output format and fields may differ between slurm versions
+			if [ -s "${OUT}" -a -s "${OUT_DELIM}" -a \( -s "${OUT_JSON}" -o "${SKIP_JSON}" \) ] ; then
+			        mkdir -p ${TSTAMP_YMD}
+                                echo ${TSTAMP} > ${TSTAMP_YMD}/${GENERATED}
+			        sinfo --version | awk '{print $2}' > ${TMPDIR}/${SLURM_VERSION}
+			        mv ${TMPDIR}/* ${TSTAMP_YMD}/
 
-	if [[ -e ${TSTAMP_YMD}/${GENERATED} ]] ; then
-		rc=10
-	else
-		# if incomplete or in error, leave behind all of the breadcrumb temporary files
-		if [[ -s ${OUT} && -s ${OUT_DELIM} && -s ${OUT_JSON} ]] ; then
-			mkdir -p ${TSTAMP_YMD}
-                        echo ${TSTAMP} > ${TSTAMP_YMD}/${GENERATED}
-			sinfo --version | awk '{print $2}' > ${TMPDIR}/${SLURM_VERSION}
-			mv ${TMPDIR}/* ${TSTAMP_YMD}/
-
-			# completed successfully, seal it
-			( cd ${TSTAMP_YMD}; chmod 0444 * )
-			trap "rm -rf ${TMPDIR}" 0
-		else
-
-			echo "${MSG_INCOMPLETE} See: ${TMPDIR}" >/dev/tty
-			rc=11
-		fi
+			        # completed successfully, seal it
+			        ( cd ${TSTAMP_YMD}; chmod ${READ_ONLY} * )
+			        trap "rm -rf ${TMPDIR}" 0
+		        else
+			        echo "${MSG_INCOMPLETE} See: ${TMPDIR}" ${ISATTY}
+			        rc=${EX_INCOMPLETE}
+		        fi
+	        fi
 	fi
 	return ${rc}
 }
 
 main() {
-	local rc=0
+	local rc=${EX_OK}
 	Prep
 	CollectSlurmData
 	ConvertDelimiters
-	CheckValid
+	CheckValid	# potentially long to return (TIMEOUT_LONG)
 	rc=$?
 	exit ${rc}
 }
@@ -184,3 +223,4 @@ main $*
 exit $?
 
 #python3 src/__main__.py -i${OUT} -t36 -n${CLUSTERNAME} -c368
+#vi: set background=dark paste
